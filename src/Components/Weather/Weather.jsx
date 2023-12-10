18b1a574812331a2089b4603d826945de616ee6f
Francisco Azvalinsky <frankyazva@gmail.com>
2023-12-09 21:58:49 -0300

    Integracion de la api del gobierno para la utilizacion de las coordenadas y nombres de los municipios de las provincias. A su vez, se utiliza para desplegar opciones de seleccionar cada una de ellas y actualizarse los datos en pantalla

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -5,29 +5,50 @@ import axios from 'axios'
 
 export default function Weather() {
   let [response, setResponse] = useState()
-  let [name, setName] = useState()
-  let [cities, setCities] = useState();
+  //let [name, setName] = useState()
+  let [provincia, setProvincia] = useState('Santa+Fe')
+  let [city, setCity] = useState({name: 'Santa+Fe', lat:'-31.655', lon:'-60.638'});
+
+  let [provincias, setProvincias] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       console.log('Fetching data')
+      let prov = provincia.replace('+', ' ')
       try {
-        let res2 = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=Miami&count=1&language=en&format=json`)
-        console.log(res2);
-       
-        let res = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${res2.data.results[0].latitude}&longitude=${res2.data.results[0].longitude}&current=temperature_2m,precipitation&minutely_15=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`);
-        console.log(res)
-        setResponse(res.data);
-        setName(res2.data.results[0].name)
+        let provNameId= await axios.get(`https://apis.datos.gob.ar/georef/api/provincias?nombre=${prov}`)
+        console.log('provNameId:')
+        console.log(provNameId)
+        let id = provNameId.data.provincias[0].id
+        let municipios= await axios.get(`https://apis.datos.gob.ar/georef/api/municipios?provincia=${id}&campos=id,nombre,centroide&max=1000`)
+        setProvincias(municipios.data.municipios)
+        console.log('municipios:')
+        console.log(municipios);
+        let cityName = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`)
+        console.log('cityName:')
+        console.log(cityName);
+        let cityClimate = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lon}&current=temperature_2m,precipitation&minutely_15=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`);
+        console.log('cityClimate:')
+        console.log(cityClimate.data)
+        setResponse(cityClimate.data);
+        //setName(cityName.data.results[0].name || city.name.replace('+', ' '))
       } catch (error) {
         console.error('Error en la solicitud:', error);
       }
     }
     fetchData();
-    
-  }, [])
 
-  console.log(response)
+  }, [city, provincia])
+
+  const setCiudad = (e) => {
+    const selectedOption = JSON.parse(e.target.value)
+    const { name, lat, lon } = selectedOption;
+    setCity({ name, lat, lon });
+  };
+
+  const setProvince = (e) => {
+    setProvincia(e.target.value)
+  }
 
   let maxTemp, minTemp;
 
@@ -61,15 +82,49 @@ export default function Weather() {
     maxTemp = maxTempArr[maxTempArr.length - 1]
     minTemp = minTempArr[0]
 
+    let municipios = provincias.map((provincia, index) => {
+      
+      let name=provincia.nombre.replace(' ', '+')
+      let lon = parseFloat(provincia.centroide.lon.toFixed(3));
+      let lat = parseFloat(provincia.centroide.lat.toFixed(3));
+
+      let value = JSON.stringify({
+        name,
+        lat,
+        lon
+      });
+
+      return (
+      <option value={value} key={index}>
+        {provincia.nombre}
+      </option>
+    )})
+
     return (
       <div>
-        <h2>El clima de {name} es:</h2>
-        <p>Temperatura: {response.current.temperature_2m} {response.current_units.temperature_2m}</p>
-        <p>Precipitaciones: {response.current.precipitation} {response.current_units.precipitation}</p>
-        <p>Temperatura mínima: {minTemp} {response.daily_units.temperature_2m_min}</p>
-        <p>Temperatura máxima: {maxTemp} {response.daily_units.temperature_2m_max}</p>
-        <p>Amanecer: {response.daily.sunrise[0].slice(11)}</p>
-        <p>Atardecer: {response.daily.sunset[0].slice(11)}</p>
+        <div>
+          <h2>El clima de {city.name.replace('+', ' ')} es:</h2>
+          <p>Temperatura: {response.current.temperature_2m} {response.current_units.temperature_2m}</p>
+          <p>Precipitaciones: {response.current.precipitation} {response.current_units.precipitation}</p>
+          <p>Temperatura mínima: {minTemp} {response.daily_units.temperature_2m_min}</p>
+          <p>Temperatura máxima: {maxTemp} {response.daily_units.temperature_2m_max}</p>
+          <p>Amanecer: {response.daily.sunrise[0].slice(11)}</p>
+          <p>Atardecer: {response.daily.sunset[0].slice(11)}</p>
+        </div>
+        <div>
+          <p>Selecciona una provincia</p>
+          <select onChange={setProvince} defaultValue='Santa+Fe'>
+            <option value='Buenos+Aires'>Buenos Aires</option>
+            <option value='Cordoba'>Cordoba</option>
+            <option value='Santa+Fe'>Santa Fe</option>
+            <option value='Mendoza'>Mendoza</option>
+          </select>
+
+          <p>Selecciona una localidad</p>
+          <select onChange={setCiudad}>
+            {municipios}
+          </select>
+        </div>
       </div>
     )
   }
